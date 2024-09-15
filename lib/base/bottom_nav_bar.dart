14e6654c961e8e2c0d0e0efd68a36b4090c6f499
LEEJINYOUN <swlsdud1005@naver.com>
2024-09-16 07:34:48 +0900

    하단 메뉴 기본 디자인

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // BottomNavBarContainer 불러오기
+    return const MaterialApp(home: BottomNavBarContainer());
+  }
+}
+
+class BottomNavBarContainer extends StatefulWidget {
+  const BottomNavBarContainer({super.key});
+
+  @override
+  // _BottomNavBarState 불러오기 => state 생성 후 반환
+  State<BottomNavBarContainer> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBarContainer> {
+  // bottom 메뉴 리스트
+  final appScreens = [
+    const Center(child: Text('Home')),
+    const Center(child: Text('Bookmark')),
+    const Center(child: Text('Profile')),
+  ];
+
+  // 선택 인덱스 초기화
+  int _selectedIndex = 0;
+
+  // bottom 메뉴 버튼 변경
+  void _onItemTapped(int index) {
+    // setState => 실시간으로 변경
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('My Tickets')),
+        ),
+        body: appScreens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            selectedItemColor: Colors.blueGrey,
+            unselectedItemColor: const Color(0xff526400),
+            showSelectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_heart_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_heart_filled),
+                  label: 'Bookmark'),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+                  label: 'Profile'),
+            ]));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_flutter_front/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Center(child: Text('테스트')),
-    );
+    // BottomNavBar 불러오기
+    return const MaterialApp(home: BottomNavBar());
   }
 }
